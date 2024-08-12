94f1672cd8d24848bc1b7d0e8e398b03b0d8e392
mbs <mbsc26@live.com>
2024-08-12 08:11:55 +0500

    initialized cors in app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)  # Initialize CORS
 
 # Load users from JSON file
 def load_users():
